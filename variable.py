28145bdabc7e8cc31d4b318d4aac45e190e4d3a4
ardyezapratama <ardyezapratama@gmail.com>
2022-12-22 14:36:01 +0800

    basic variable.py

---
+++ variable.py
@@ -0,0 +1,30 @@
+# Variable adalah tempat menyimpan data
+
+# menyimpan / assignment nilai
+a = 10
+b = 5
+panjang = 10000
+
+#pemanggilan pertama
+print("Nilai a = ", a)
+print("Nilai b = ", b)
+print("Nilai Panjang = ", panjang)
+
+#penamaan variable
+nilai_x = 20 #dengan menggunakan underscore
+mill20 = 20000000 
+nilaiI = 1.5
+
+# pemanggilan kedua
+print("Nilai a = ", a )
+a = 20
+print("Nilai a = ", a)
+
+# Assignment inderect
+b = a
+print("Nilai b = ", b)
+
+
+
+
+
