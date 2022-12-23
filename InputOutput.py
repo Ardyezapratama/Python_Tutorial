77cf5e14ec1fcca3a36c65fe7bd0faa650e1a750
ardyezapratama <ardyezapratama@gmail.com>
2022-12-23 17:49:21 +0800

    Input Output

---
+++ InputOutput.py
@@ -0,0 +1,18 @@
+# Input User
+
+#data yang dimasukkan pasti string
+data = input("Masukkan data: ")
+print("Data = ", data, ", Type: ", type(data))
+
+# casting ke int
+data_int = int(input("Masukkna angka: "))
+print("Data: ", data_int, ", Type: ", type(data_int))
+
+# casting ke float
+number = float(input("Masukkan angka: "))
+print("Data: ", number, ", Type: ", type(number))
+
+# casting ke boolean
+biner = bool(int(input("Massukkan nilai boolean: ")))
+print("data = ", biner, ", type: ", type(biner))
+
