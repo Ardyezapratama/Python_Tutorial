7071612ada473d7d776ed5b0eb40dbee56895267
ardyezapratama <ardyezapratama@gmail.com>
2022-12-23 18:10:15 +0800

    Arithmetic Operation

---
+++ arithmeticOperation.py
@@ -0,0 +1,42 @@
+# Operasi Aritmatika
+
+a = 10
+b = 3
+
+# Operasi penjumlahan +
+hasil = a + b
+print("a + b = ", hasil)
+
+# Operasi pengurangan - 
+hasil = a - b
+print("a - b = ",hasil)
+
+# Operasi perkalian *
+hasil = a * b
+print("a * b = ",hasil)
+
+# Operasi pembagian /
+hasil = a / b
+print("a / b = ",hasil)
+
+# Operasi eksponen (pangkat) **
+hasil = a ** b
+print("a ** b = ", hasil)
+
+# Operasi modulus %
+hasil = a % b
+print("a % b = ", hasil)
+
+# Operasi floor division //
+hasil = a // b
+print("a // b = ", hasil)
+
+
+# Prioritas operasi, operational precendence
+# Urutan prioritas (), **, *, /, %, //, +, -
+x = 3
+y = 2
+z = 4
+
+hasil = 3 ** 2 * 4 + 3 / 2 - 2 % 4 // 3
+print("3 ** 2 * 4 + 3 / 2 - 2 % 4 // 3 = ",hasil)
